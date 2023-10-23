41ac22b728290f0a0ba7fe87ad3176440e60f06a
angus <angusmiguel21@gmail.com>
2023-10-23 01:46:19 +0100

    cwd as target path prettier

--- generatePrettierConfig.js
+++ generatePrettierConfig.js
@@ -2,9 +2,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const targetPath = path.resolve(__dirname.split("node_modules")[0]);
-
 fs.copyFileSync(
 	path.resolve(__dirname, "./prettierrc.js"),
-	path.resolve(targetPath, "prettierrc.js")
+	path.resolve(process.cwd(), "prettierrc.js")
 );
